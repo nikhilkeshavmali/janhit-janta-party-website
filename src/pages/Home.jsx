d512221704691e84d8218dd843e1fc017bdf05a1
Nikhil Mali <168334612+nikhilkeshavmali@users.noreply.github.com>
2026-08-29 18:59:53 +0530

    Render all home page sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,23 @@ import PartyIntro from "../components/home/PartyIntro";
 import Highlights from "../components/home/Highlights";
 import FeaturedInitiatives from "../components/home/FeaturedInitiatives";
 import LatestUpdates from "../components/home/LatestUpdates";
+import NationalMessage from "../components/home/NationalMessage";
+import VisionMission from "../components/home/VisionMission";
+import KeyInitiatives from "../components/home/KeyInitiatives";
+import JoinBanner from "../components/home/JoinBanner";
 
 const Home = () => {
   return (
     <main className="home-page">
       <HeroSection />
       <PartyIntro />
+      <NationalMessage />
       <Highlights />
+      <VisionMission />
+      <KeyInitiatives />
       <FeaturedInitiatives />
       <LatestUpdates />
+      <JoinBanner />
     </main>
   );
 };
