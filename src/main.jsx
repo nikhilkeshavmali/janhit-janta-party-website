eb953f04f27dae7774437b701581b19fb8bcc1f7
pawarsakshi2004 <pawarsakshisharad07@gmail.com>
2026-08-30 01:22:59 +0530

    Fix syntax in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-<<<<<<< HEAD
   </StrictMode>
 )
-=======
   </React.StrictMode>,
 );
->>>>>>> 05bc48b9b50aeeade8fc4b9448745d4fda398643
