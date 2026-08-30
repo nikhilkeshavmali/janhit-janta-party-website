24b284129ca973871db0bed14192c3fdea12ca0e
Nikhil Mali <168334612+nikhilkeshavmali@users.noreply.github.com>
2026-08-30 10:12:56 +0530

    Resolve App routing conflict and add Events route

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,5 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
-import Events from './pages/Events.jsx' 
-
-function App() {
-  // const [count, setCount] = useState(0)
-
-  return (
-    <div>
-          <Events />
-    </div>
-  )
-}
-
-export default App
-
-
-=======
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import MainLayout from "./layouts/MainLayout";
 
 import Home from "./pages/Home";
@@ -30,6 +7,7 @@ import About from "./pages/About";
 import VisionMission from "./pages/VisionMission";
 import Donor from "./pages/Donor";
 import News from "./pages/News";
+import Events from "./pages/Events";
 
 function App() {
   return (
@@ -41,6 +19,7 @@ function App() {
           <Route path="/vision-mission" element={<VisionMission />} />
           <Route path="/donor" element={<Donor />} />
           <Route path="/news" element={<News />} />
+          <Route path="/events" element={<Events />} />
         </Route>
       </Routes>
     </BrowserRouter>
@@ -48,4 +27,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 05bc48b9b50aeeade8fc4b9448745d4fda398643
